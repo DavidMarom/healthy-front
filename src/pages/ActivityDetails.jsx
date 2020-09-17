3a6360748f586aa65ca6d5048a81fbffc6845e71
chen edri <chenedri22@gmail.com>
2020-09-17 19:51:40 +0300

    awdasd

--- src/pages/ActivityDetails.jsx
+++ src/pages/ActivityDetails.jsx
@@ -16,7 +16,7 @@ export class _ActivityDetails extends Component {
         "https://res.cloudinary.com/dygtul5wx/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1600327803/sprint%204/users/74_cludfc.jpg",
     },
     creator: "",
-  };
+  }
 
   componentDidMount() {
     this.loadActivity();
@@ -28,16 +28,16 @@ export class _ActivityDetails extends Component {
     activityService.getById(activityId).then((activity) => {
       this.setState({ activity }, () =>
         this.loadCreator(activity.createdBy._id)
-      );
-    });
-  };
+      )
+    })
+  }
 
   loadCreator = (id) => {
     userService.getById(id).then((creator) => {
       console.log("creator", creator);
       this.setState({ creator });
-    });
-  };
+    })
+  }
 
   purchaseActivity(activity, user, creator) {
     creator.income += activity.price;
@@ -61,127 +61,72 @@ export class _ActivityDetails extends Component {
         <div className="event-main-container">
           <div className="event-left-side">
             <div className="event-creator-container">
-              
+
 
               <div>
                 <h2>{activity.createdBy.fullName}</h2>
               </div>
 
-<<<<<<< HEAD
-              <div>
-                <img
-                  className="creator-img"
-                  src={activity.createdBy.imgUrl}
-                />
-              </div>
-
-            </div>
-          </div>
-          <div className="event-right-side"></div>
-        </div>
-
-        <div className="main-info-container flex sb">
-          <div className="main-info-card flex column">
-            <h3 className="det-name f28 fw6">{activity.createdBy.fullName}</h3>
-            <h5>{activity.location.address}</h5>
-            <p className="fs18 fw4 clr6">{activity.description}</p>
-          </div>
-          <div className="right-payment-area flex column sa">
-            <div className="payment-det">
-              <h4>Price: ${activity.price}</h4>
-              <button
-                onClick={() => this.purchaseActivity(activity, user, creator)}
-              >
-                Buy this ITEM!
-              </button>
-            </div>
-            <div className="attendings">
-              <h3>Attending</h3>
-              {activity.participants.map((participant, idx) => (
-                <img
-                  className="attending-img"
-                  key={idx}
-                  src={participant.imgUrl}
-                />
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="lower-info-area flex">
-          <div className="flex column sa prefs">
-            <h2 className="border-bottom">properties</h2>
-            <div className="flex sa wrap">
-              {activity.tags.map((tag, idx) => (
-                <li key={idx}>{tag}</li>
-              ))}
-            </div>
-            <div className="tac">⭐⭐⭐⭐⭐</div>
-            {/* <div className="rev-det">Reviews
-=======
     render() {
-        const { activity, user, creator } = this.state;
+        const {activity, user, creator} = this.state;
         if (!activity) return <h1>Loading...</h1>
         return (
             <div className="main-details-card">
                 <h2 className="f20 title">{activity.title}</h2>
                 <div className="image-gallery">
-                    {activity.imgUrls.map((img, idx) => <img className={`img${idx}`} key={idx} src={img} />)}
+                  {activity.imgUrls.map((img, idx) => <img className={`img${idx}`} key={idx} src={img} />)}
                 </div>
                 <div className="main-info-container flex sb">
-                    <div className="main-info-card flex column">
-                        <h3 className="det-name f28 fw6">{activity.createdBy.fullName}</h3>
-                        <h5>{activity.location.address}</h5>
-                        <p className="fs18 fw4 clr6">{activity.description}</p>
+                  <div className="main-info-card flex column">
+                    <h3 className="det-name f28 fw6">{activity.createdBy.fullName}</h3>
+                    <h5>{activity.location.address}</h5>
+                    <p className="fs18 fw4 clr6">{activity.description}</p>
+                  </div>
+                  <div className="right-payment-area flex column sa">
+                    <div className="payment-det">
+                      <h4>Price: ${activity.price}</h4>
+                      <button onClick={() => this.purchaseActivity(activity, user, creator)}>Buy this ITEM!</button>
                     </div>
-                    <div className="right-payment-area flex column sa">
-                        <div className="payment-det">
-                            <h4>Price: ${activity.price}</h4>
-                            <button onClick={()=>this.purchaseActivity(activity,user, creator)}>Buy this ITEM!</button>
-                        </div>
-                        <div className="attendings">
-                            <h3>Attending</h3>
-                            {activity.participants.map((participant, idx) => <img className="attending-img"key={idx} src={participant.imgUrl} />)}
-                        </div>
+                    <div className="attendings">
+                      <h3>Attending</h3>
+                      {activity.participants.map((participant, idx) => <img className="attending-img" key={idx} src={participant.imgUrl} />)}
                     </div>
+                  </div>
                 </div>
                 <div className="lower-info-area flex">
-                    <div className="flex column sa prefs">
-                        <h2 className="border-bottom:">properties</h2>
-                        <div className="flex sa wrap">
-                            {activity.tags.map((tag, idx) => <li key={idx}>{tag}</li>)}
-                        </div>
-                        <div className="tac">⭐⭐⭐⭐⭐</div>
-                        {/* <div className="rev-det">Reviews
->>>>>>> 5ca0dbc7c8df7150d61f01143f7d56e4ed698b23
+                  <div className="flex column sa prefs">
+                    <h2 className="border-bottom:">properties</h2>
+                    <div className="flex sa wrap">
+                      {activity.tags.map((tag, idx) => <li key={idx}>{tag}</li>)}
+                    </div>
+                    <div className="tac">⭐⭐⭐⭐⭐</div>
+                    {/* <div className="rev-det">Reviews
                         {activity.reviews.map((review, idx)=> {
                         <ul key={idx}>
                             <li>{review}</li>
                         </ul>})
     }
                         </div> */}
-          </div>
-          <div className="fitchers-info flex column">
-            <div className="box-area">CHAT AREA</div>
-            <div>MAP AREA</div>
-          </div>
-        </div>
-      </div>
-    );
+                  </div>
+                  <div className="fitchers-info flex column">
+                    <div className="box-area">CHAT AREA</div>
+                    <div>MAP AREA</div>
+                  </div>
+                </div>
+              </div>
+    )
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    activities: state.activityReducer.activities,
+                activities: state.activityReducer.activities,
     // user: state.userReducer.loggedinUser;
-  };
-};
+  }
+}
 const mapDispatchToProps = {
-  saveActivity,
-  updateUser,
-};
+                saveActivity,
+              updateUser,
+}
 
-export const ActivityDetails = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(_ActivityDetails);
+export const ActivityDetails = connect(mapStateToProps,mapDispatchToProps)(_ActivityDetails);
