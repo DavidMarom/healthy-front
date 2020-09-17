e3c42ff69522a86aa0954cd0d82785118cf6f9b3
LAPTOP-TO9F8GT3\daniel <d.bar6531@gmail.com>
2020-09-17 20:16:52 +0300

    commit

---
+++ src/cmps/activity/SearchBox.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+ render() {
+    return (
+
+        <div className="inbox-nav">
+            <input className="email-search" type="text" placeholder="search mail" onChange={this.handleChange} />
+        </div>
+    )
+
+}
