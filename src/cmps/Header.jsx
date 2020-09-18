7b6bbea5c59326836a11cb24a50bb7cdc9f85e34
David Marom <davidmarom@hotmail.com>
2020-09-19 00:19:53 +0300

    merging

--- src/cmps/Header.jsx
+++ src/cmps/Header.jsx
@@ -1,67 +1,55 @@
-<<<<<<< HEAD
-import { NavLink, withRouter } from 'react-router-dom'
-import React, { Component } from 'react'
-
-import { SearchBox } from './activity/SearchBox.jsx'
-import eventBus from '../services/event-bus-service.js'
-
-=======
-import { NavLink, withRouter } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import React, { Component } from "react";
+
 import { SearchBox } from "./activity/SearchBox.jsx";
 import eventBus from "../services/event-bus-service.js";
->>>>>>> 0b94e9f25bf02f481b9361472bb00741a0565e70
 
 export class Header extends Component {
+  state = {
+    isHomePage: false,
+  };
 
-    state = {
-        isHomePage: false
-    }
+  unsubscribeHome;
+  unsubscribeOutOfHome;
 
-    unsubscribeHome;
-    unsubscribeOutOfHome;
+  componentDidMount() {
+    this.unsubscribeHome = eventBus.on("homePage", () => {
+      this.setState({ isHomePage: true }, console.log("im home"));
+    });
 
-    componentDidMount() {
-        this.unsubscribeHome = eventBus.on('homePage', () => {
-            this.setState({ isHomePage: true }, console.log('im home'))
-        })
+    this.unsubscribeOutOfHome = eventBus.on("out of homePage", () => {
+      this.setState({ isHomePage: false }, console.log("out of homePage"));
+    });
+  }
 
-        this.unsubscribeOutOfHome = eventBus.on('out of homePage', () => {
-            this.setState({ isHomePage: false }, console.log('out of homePage'))
-        })
-    }
+  componentWillUnmount() {
+    this.unsubscribeHome();
+    this.unsubscribeOutOfHome();
+  }
 
-    componentWillUnmount() {
-        this.unsubscribeHome()
-        this.unsubscribeOutOfHome()
-    }
+  render() {
+    const { isHomePage } = this.state;
+    return (
+      <header className="main-header">
+        <div className="left-end">
+          <div className="logo">
+            <NavLink to="/">Logo</NavLink>
+          </div>
+          <div>
+            <NavLink to="/activity">Explore</NavLink>
+          </div>
+        </div>
 
-    render() {
-        const { isHomePage } = this.state
-        return <header className="main-header">
-            <div className="left-end">
-                <div className="logo">
-                    <NavLink to="/">Logo</NavLink>
-                </div>
-                <div>
-                    <NavLink to="/activity">Explore</NavLink>
-                </div>
-            </div>
+        {!isHomePage && <SearchBox cssClass={"header-search"} />}
 
-            {!isHomePage && <SearchBox cssClass={'header-search'} />}
-
-            <div className="right-end">
-                <div>
-                    <NavLink to={`/login`}>Login</NavLink>
-                    <NavLink to={`/signUp`}>SignUp</NavLink>
-                    <NavLink to={`/user`}>UserProfile</NavLink>
-                </div>
-            </div>
-
-        </header>
-    }
+        <div className="right-end">
+          <div>
+            <NavLink to={`/login`}>Login</NavLink>
+            <NavLink to={`/signUp`}>SignUp</NavLink>
+            <NavLink to={`/user`}>UserProfile</NavLink>
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 0b94e9f25bf02f481b9361472bb00741a0565e70
