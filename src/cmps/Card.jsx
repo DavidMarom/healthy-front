68875ad223969cff03fd1f09407b90dbe3e9ce66
LAPTOP-TO9F8GT3\daniel <d.bar6531@gmail.com>
2020-09-18 12:22:02 +0300

    eventbussearch

--- src/cmps/Card.jsx
+++ src/cmps/Card.jsx
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom'
 
 export function Card(props) {
   const activity=props.activity;
-  console.log(activity);
   return (
     <section><Link to ={`activity/${activity._id}`}>
       <div className="card">
--- src/cmps/Header.jsx
+++ src/cmps/Header.jsx
@@ -2,44 +2,49 @@ import { NavLink, withRouter } from 'react-router-dom'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { SearchBox } from './activity/SearchBox.jsx'
+import eventBus from '../services/event-bus-service.js'
 
 
 export class Header extends Component {
-   
-    state={
-        isHomePage:false
+
+    state = {
+        isHomePage: false
     }
+
+    unsubscribeHome;
+    unsubscribeOutOfHome;
 
     componentDidMount() {
-    //    const currUrl=window.location.href
-    //    if(currUrl== 'http://localhost:3000/#/'){
-    //        this.setState({isHomePage: true})
-    //    }
+        this.unsubscribeHome = eventBus.on('homePage', () => {
+            this.setState({ isHomePage: true }, console.log('im home'))
+        })
 
-    //    if(!this.props.match){
-    //     this.setState({isHomePage: true})
-    //    }
-       
+        this.unsubscribeOutOfHome = eventBus.on('out of homePage', () => {
+            this.setState({ isHomePage: false }, console.log('out of homePage'))
+        })
     }
-    
 
-    render(){
-       const {isHomePage}=this.state
+    componentWillUnmount() {
+        this.unsubscribeHome()
+        this.unsubscribeOutOfHome()
+    }
+
+    render() {
+        const { isHomePage } = this.state
         return <header className="main-header">
-        <div className="logo">
-            <NavLink to="/">Home</NavLink>
-        </div>
-        <div>
-            <NavLink to="/activity">Explore</NavLink>
-        </div>
-     { !isHomePage && <SearchBox /> }
-        
-        <div>
-            <NavLink to={`/user`}>UserProfile</NavLink>
-        </div>
-    </header>
+            <div className="logo">
+                <NavLink to="/">Home</NavLink>
+            </div>
+            <div>
+                <NavLink to="/activity">Explore</NavLink>
+            </div>
+            {!isHomePage && <SearchBox />}
+
+            <div>
+                <NavLink to={`/user`}>UserProfile</NavLink>
+            </div>
+        </header>
     }
-   
 }
 
 
@@ -50,7 +55,7 @@ export class Header extends Component {
 
 
 // export function Header(props) {
-    
+
 //     return <header className="main-header">
 //         <div className="logo">
 //             <NavLink to="/">Home</NavLink>
@@ -59,7 +64,7 @@ export class Header extends Component {
 //             <NavLink to="/activity">Explore</NavLink>
 //         </div>
 //       <SearchBox />
-        
+
 //         <div>
 //             <NavLink to={`/user`}>UserProfile</NavLink>
 //         </div>
