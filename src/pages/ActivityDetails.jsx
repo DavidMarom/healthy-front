7c278da0a6ab4da065e8750e32b9a5893d52fe5b
David Marom <davidmarom@hotmail.com>
2020-09-18 18:58:57 +0300

    activity page images css bug fix

--- src/pages/ActivityDetails.jsx
+++ src/pages/ActivityDetails.jsx
@@ -63,7 +63,6 @@ export class _ActivityDetails extends Component {
             <img className={`img${idx} gallery__img`} key={idx} src={img} />
           ))}
         </div>
-
         <div className="event-main-container">
           {/* LEFT SIDE */}
           <div className="event-left-side">
@@ -103,9 +102,8 @@ export class _ActivityDetails extends Component {
               ))}
             </div>
             <div className="divider d-hi"></div>
-<p>Rate</p>
+            <p>Rate</p>
             <div className="tac">⭐⭐⭐⭐⭐</div>
-
           </div>
 
           {/* RIGHT SIDE */}
@@ -141,14 +139,14 @@ export class _ActivityDetails extends Component {
                 />
               ))}
             </div>
-            <img src={require('../assets/img/map.jpg')} />
+            <img src={require("../assets/img/map.jpg")} />
           </div>
           {/* END OF RIGHT SIDE */}
         </div>
-        <div className="divider d-hi"></div>
 
-        
       </div>
+
+      
     );
   }
 }
