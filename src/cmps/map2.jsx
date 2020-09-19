ef37b73b0af257b5906003f55425b44203720d59
David Marom <davidmarom@hotmail.com>
2020-09-19 22:58:55 +0300

    css work

--- src/cmps/map2.jsx
+++ src/cmps/map2.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
  
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
 const AnyReactComponent = () => <div><img src='https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png' style={{ height:40, width:40}} /></div>
 
 const API_KEY = 'AIzaSyCTwmmUbksAqfSEKLn9fR4oSVbBimBrXvk'
