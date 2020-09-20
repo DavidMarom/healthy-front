7c3f3afcb90e2ab9169fa53c78749089602d8a19
LAPTOP-TO9F8GT3\daniel <d.bar6531@gmail.com>
2020-09-20 12:02:42 +0300

    edit

--- src/services/imgUploadService.js
+++ src/services/imgUploadService.js
@@ -3,18 +3,24 @@ export async function uploadImg(ev) {
     const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
 
     const formData = new FormData();
-    formData.append('file', ev.target.files[0])
-    formData.append('upload_preset', 'iqbe0osh');
-    try {
-        const res = await fetch(UPLOAD_URL, {
-            method: 'POST',
-            body: formData
-        })
-        const data = await res.json()
-        console.log(data);
-        return data
+    const files = ev.target.files
+    let dataArr = []
+    for (const file of files) {
+        formData.append('file', file)
+        formData.append('upload_preset', 'iqbe0osh');
+        try {
+            const res = await fetch(UPLOAD_URL, {
+                method: 'POST',
+                body: formData
+            })
+            const data = await res.json()
+            dataArr.push(data)
 
-    } catch (err) {
-        console.log(err);
+        } catch (err) {
+            console.log(err);
+        }
     }
+    const dataUrls = dataArr.map((data) => { return data.url })
+    return dataUrls
+
 }
