060090983bec5d849e744c1fde50a79ae79279b7
David Marom <davidmarom@hotmail.com>
2020-09-22 14:28:32 +0300

    Chat restored, css ,design

--- src/cmps/Reviews.jsx
+++ src/cmps/Reviews.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {activityService} from "../services/activityService"
+import { activityService } from "../services/activityService"
 
 export class Reviews extends Component {
   state = {
@@ -22,14 +22,14 @@ export class Reviews extends Component {
   addReview = (ev) => {
     ev.preventDefault();
     var newActivity = this.state.activity;
-    const tmpReview={
+    const tmpReview = {
       "id": Date.now(),
       "txt": this.state.txt,
       "rate": 5,
       "by": this.props.user
     }
-    this.setState({txt:''})
-    this.setState({activity:newActivity})
+    this.setState({ txt: '' })
+    this.setState({ activity: newActivity })
     newActivity.reviews.push(tmpReview);
     activityService.update(newActivity);
   }
@@ -40,16 +40,8 @@ export class Reviews extends Component {
     return (
       <section>
         <h2> Reviews</h2>
-        {reviews.map((review, idx) => {
-          return (
-            <div key={idx} className="text-mid-left">
-              <img src={review.by.imgUrl} className="attending-img marg-5" alt="" />
-              {review.txt}
-            </div>
-          );
-        })}
-        <div className="d-hi"></div>
-        <h3>Add your own review:</h3>
+
+        <p>Add your own review:</p>
 
         <form onSubmit={this.addReview}>
           <input
@@ -62,6 +54,18 @@ export class Reviews extends Component {
             <i className="far fa-paper-plane fa-2x"></i>
           </button>
         </form>
+
+        <div className="d-hi"></div>
+
+        {reviews.map((review, idx) => {
+          return (
+            <div key={idx} className="text-mid-left">
+              <img src={review.by.imgUrl} className="attending-img marg-5" alt="" />
+              {review.txt}
+            </div>
+          );
+        })}
+
       </section>
     );
   }
