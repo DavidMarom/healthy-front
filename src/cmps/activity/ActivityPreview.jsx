f36486c331eab1cb6835743c7db3a53145007cdf
chen edri <chenedri22@gmail.com>
2020-09-23 19:24:21 +0300

    thehe

--- src/cmps/activity/ActivityPreview.jsx
+++ src/cmps/activity/ActivityPreview.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-// import { activityService } from '../../services/activityService'
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+// // import { activityService } from '../../services/activityService'
 
-export function ActivityPreview({ activity }) {
-    return (
-        <div className="activity-preview cardnew">
-            <Link to={`activity/${activity._id}`}>
-                <div className="card-img">
-                    <img className="img-fit" src={activity.imgUrls[0]} alt="" />
-                </div>
-                <h4>{activity.title}</h4>
-                <li>{activity.startsAt}</li>
-                <img src={activity.createdBy.imgUrl}/>
-                <p>{activity.subtitle}</p>
-                <li>{activity.location.address}</li>
-                <h5>{activity.createdBy.fullName}</h5>
-                <li>${activity.price}</li>
-            </Link>
-        </div>
-    )
-}
+// export function ActivityPreview({ activity }) {
+//     return (
+//         <div className="activity-preview cardnew">
+//             <Link to={`activity/${activity._id}`}>
+//                 <div className="card-img">
+//                     <img className="img-fit" src={activity.imgUrls[0]} alt="" />
+//                 </div>
+//                 <h4>{activity.title}</h4>
+//                 <li>{activity.startsAt}</li>
+//                 <img src={activity.createdBy.imgUrl}/>
+//                 <p>{activity.subtitle}</p>
+//                 <li>{activity.location.address}</li>
+//                 <h5>{activity.createdBy.fullName}</h5>
+//                 <li>${activity.price}</li>
+//             </Link>
+//         </div>
+//     )
+// }
 
