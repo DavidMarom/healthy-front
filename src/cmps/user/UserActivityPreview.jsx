4d1b59eecbfcc30447124c5ca8aa927fb6f36b41
David Marom <davidmarom@hotmail.com>
2020-09-23 04:30:29 +0300

    profile page - advanced

--- src/cmps/user/UserActivityPreview.jsx
+++ src/cmps/user/UserActivityPreview.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 // import { activityService } from '../../services/activityService'
 
-export function UserActivityPreview({ activity, user, madeOfOperation, onRemove, onRemoveFromList }) {
+export class UserActivityPreview extends Component {
 
-    function renderDay(value) {
+    state = {
+        pin: false,
+        sign: '+'
+    }
+
+    componentDidMount() {
+        let { activity, user, madeOfOperation, onRemove, onRemoveFromList } = this.props;
+        this.setState({ activity: activity })
+    }
+
+    renderDay = (value) => {
         let res = "";
         switch (value) {
             case 1:
@@ -41,45 +51,70 @@ export function UserActivityPreview({ activity, user, madeOfOperation, onRemove,
         return res;
     }
 
-    function showActivityDetails(ev){
+    showActivityDetails = (ev) => {
         ev.preventDefault();
         ev.stopPropagation();
+        this.setState({ pin: !this.state.pin })
+        this.state.pin ?
+            this.setState({ sign: '+' }) :
+            this.setState({ sign: '-' })
+
     }
 
-    return (
-        <Link to={`activity/${activity._id}`}>
-            <div className="user-activity-preview flex">
-                <img src={activity.imgUrls[0]} alt="" />
-                <div className="flex column">
-                    <h4 className="fs18">{activity.title}</h4>
-                    <li className="fs14">{activity.location.address}</li>
-                </div>
+    render() {
+        let { activity, user, madeOfOperation, onRemove, onRemoveFromList } = this.props;
+        if (!activity) return <h1>loading</h1>
+        return (
+            <section className="bg-white">
+                <Link to={`activity/${activity._id}`}>
 
-                {(madeOfOperation === 'organizer') ? (<Link to={`/activity/edit/${activity._id}`}>Edit</Link>) : ''}
-                {(madeOfOperation === 'organizer') ? (<button onClick={(ev) => onRemove(ev, activity._id)}>Remove the event</button>) : ''}
-                {(madeOfOperation === 'subscriber') ? (<button onClick={(ev) => onRemoveFromList(ev, activity, user)}>Remove from Your List</button>) : ''}
-                <button onClick={(ev) => showActivityDetails(ev)}>arrow</button>)
-                <section className="activity-info">
-                    <div className="day-time flex">
-                        <div>{`${renderDay(activity.dayInWeek)}-${activity.hour}:00`}</div>
-                        <div>
+                    <div className="act-strip">
+                        <div className="act-top strip">
+
+                            <div className="img-40"><img src={activity.imgUrls[0]} alt="" /></div>
+                            <div className="flex column event-title-dash">
+                                <div>{activity.title}</div>
+                                <div>{activity.location.address}</div>
+                            </div>
+
+
+                            <div className="day-time flex column">
+                                <div>{`${this.renderDay(activity.dayInWeek)}`}</div>
+                                <div>{`${activity.hour}:00`}</div>
+                            </div>
+
+                            <div className="just-row">
+                                {(madeOfOperation === 'organizer') ? (<Link to={`/activity/edit/${activity._id}`}><i className="fas fa-user-cog"></i></Link>) : ''}
+                                <div className="pad-10"></div>
+                                {(madeOfOperation === 'organizer') ? (<button className="dash-btn" onClick={(ev) => onRemove(ev, activity._id)}><i className="far fa-trash-alt"></i></button>) : ''}
+                            
+                                {(madeOfOperation === 'subscriber') ? (<button onClick={(ev) => onRemoveFromList(ev, activity, user)}>Remove from Your List</button>) : ''}
+                            </div>
+
+                            
+
+                            <div className="dash-right">
+                                <button className="dash-btn" onClick={(ev) => this.showActivityDetails(ev)}>{this.state.sign}</button>
+                            </div>
+                        </div>
+
+                        <div className={`${this.state.pin ? "pin-on" : "pin-off"}`}>
                             {(madeOfOperation === 'organizer') && (
                                 activity.participants.map((participant, idx) => {
                                     return (<div className="participant-info" key={idx}>
                                         <Link to={`/user/${participant._id}`}>
-                                            <div className="flex">
-                                                <div>{participant.fullName}</div>
-                                                <img src={participant.imgUrl} alt=""/>
+                                            <div className="dash-attendie">
+                                                <div className=".nav-override-color fs-10">{participant.fullName}</div>
+                                                <img className="attending-img" src={participant.imgUrl} alt="" />
                                             </div>
                                         </Link>
                                     </div>)
-                                }))}   
+                                }))}
                         </div>
                     </div>
-
-                </section>
-            </div>
-        </Link>
-    )
+                </Link>
+            </section>
+        )
+    }
 }
 
