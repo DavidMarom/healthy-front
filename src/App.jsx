fb8c2923cc665df9257c89f4dd1e508dea9fdddc
Eran Ben-Moha <eranb@monday.com>
2020-09-26 14:26:39 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { Component } from "react";
 import { Switch, Route } from 'react-router-dom';
 import routes from './routes.js'
 import {Header} from './cmps/Header.jsx'
 import {Footer} from './cmps/Footer.jsx'
 import {withRouter} from 'react-router'
 
-const App = (props) => {
-  const isHomepage = props.location.pathname === "/";
 
-  return (
+class _App extends Component {
+  render() {
+    const isHomepage = this.props.location.pathname === "/";
+
+    return (
+    
+      <div className="App">
+        <Header isHomepage={isHomepage}/>
+        <div className="marg-top-80"></div>
   
-    <div className="App">
-      <Header isHomepage={isHomepage}/>
-      <div className="marg-top-80"></div>
-
-      <Switch>
-      { routes.map(route => <Route key={ route.path } exact component={ route.component } path={ route.path } />) }
-      </Switch>
-      <Footer />
-    </div>
-  );
+        <Switch>
+        { routes.map(route => <Route key={ route.path } exact component={ route.component } path={ route.path } />) }
+        </Switch>
+        <Footer />
+      </div>
+    );
+  };
+  
 };
 
-export default withRouter(App);
+export default withRouter(_App);
